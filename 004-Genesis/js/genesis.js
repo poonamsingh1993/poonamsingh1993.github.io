098e43369c4e04aa39c03026d66f2bc4b7928366
poonamsingh1993 <30832505+poonamsingh1993@users.noreply.github.com>
2017-08-08 17:49:09 +0530

    Add files via upload

---
+++ 004-Genesis/js/genesis.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("div.tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.tab>div.tab-content").removeClass("active");
+        $("div.tab>div.tab-content").eq(index).addClass("active");
+    });
+});
+  $('#myCarousel1').carousel({
+  interval: 4000
+})
+
+$('#myCarousel1 .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
