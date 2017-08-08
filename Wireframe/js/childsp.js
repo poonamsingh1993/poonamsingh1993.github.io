c8330c42c60e7a61701e777496cc26aaeaff701e
poonamsingh1993 <30832505+poonamsingh1993@users.noreply.github.com>
2017-08-08 17:57:33 +0530

    Add files via upload

---
+++ Wireframe/js/childsp.js
@@ -0,0 +1,24 @@
+
+ function collapseNavbar(){
+ 	if($(".navbar").offset().top>50){
+ 		$(".navbar-fixed-top").addClass("top-nav-collapse");
+ 	}
+ 	else{$(".navbar-fixed-top").removeClass("top-nav-collapse");
+ 	}}
+
+ 	$(window).scroll(collapseNavbar);
+ 	$(document).ready(collapseNavbar);
+ 	$('.navbar-collapse ul li a').click(function(){
+ 		$(".navbar-collapse").collapse('hide');
+ 	});
+  $(".intro-body a").on('click', function(event) {
+ 	 	if(this.hash!=''){
+ 	 		console.log(this.hash);
+ 	 	var hash = this.hash;
+     $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 400, function(){
+        window.location.hash = hash;
+      });
+ 	}
+  });
